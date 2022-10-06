4b3c008c4fb5257908df9589aebd1cb11dbe652d
Kris Warner <kdwarn@protonmail.com>
2022-10-05 23:05:58 -0400

    Initial commit
    Rough approximation of layout of Marc's frontend.

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use yew::prelude::*;
+
+struct Model {}
+
+enum Msg {}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Model {}
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+                <header>
+                    <a href="https://sites.google.com/a/dvrpc.org/webteam/">{ "Intranet" }</a>
+                </header>
+                <main>
+                    <div id="left">
+                        <h1>{ "Tracking Progress Updates" }</h1>
+                        <p>{ "Add an overlay to indicator tiles to let users know they have recently been updated with new data." }</p>
+                        <ul>
+                            <li>{ " Use the 'Add' form to add an overlay for a newly updated indicator. These overlays expire 30 days after their creation."}</li>
+                            <li>{ " Use the 'Remove' form if you want to remove an indicator overlay before the 30 day expiration date. "}</li>
+                        </ul>
+                    </div>
+                    <div id="right">
+                        <h1>{ "Updated Indicators" }</h1>
+                        <p>{ "[Display of current updated indicators here (or message saying none)]"}</p>
+                        <hr />
+                        <button>{ "Add Indicator" }</button>
+                        <button>{ "Remove Indicator" }</button>
+                    </div>
+                </main>
+            </>
+        }
+    }
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        true
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
+}
