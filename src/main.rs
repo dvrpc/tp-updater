7d7e3d562014fdce7e3288bc09007890d0fe36d7
Kris Warner <kdwarn@protonmail.com>
2022-10-07 19:47:35 -0400

    Populate dropdown, handle messages, tweak styles

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,65 @@
+use gloo_console::log;
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
-struct Model {}
-
-enum Msg {}
-
-enum Indicators {
-    AirQuality,
-    BridgeConditions,
-    CommuteMode,
-    Congestion,
-    EducationalAttainment,
-    ExportedGoods,
-    GlobalConnectivity,
-    GreenhouseGasEmissions,
-    HousingActivity,
-    HousingAffordability,
-    IncomeDisparities,
-    Innovataion,
-    JobGrowth,
-    LandConsumption,
-    MilesDriven,
-    PavementConditions,
-    PopulationGrowth,
-    RacialAndEthnicDisparies,
-    RoadwaySafety,
-    SexDisparities,
-    TransitConditions,
-    TransitRidership,
-    WaterQuality,
+enum Msg {
+    Add,
+    Remove,
+    Select(String),
 }
+
+struct Model {
+    indicator: Option<String>,
+}
+
+const INDICATORS: &[&str] = &[
+    "Air Quality",
+    "Bridge Conditions",
+    "Commute Mode",
+    "Congestion",
+    "Educational Attainment",
+    "Exported Goods",
+    "Global Connectivity",
+    "Greenhouse Gas Emissions",
+    "Housing Activity",
+    "Housing Affordability",
+    "Income Disparities",
+    "Innovataion",
+    "Job Growth",
+    "Land Consumption",
+    "Miles Driven",
+    "Pavement Conditions",
+    "Population  Growth",
+    "Racial and Ethnic Disparies",
+    "Roadway Safety",
+    "Sex Disparities",
+    "Transit Conditions",
+    "Transit Ridership",
+    "Water Quality",
+];
 
 impl Component for Model {
     type Message = Msg;
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Model {}
+        Model { indicator: None }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+
+        let onchange = link.callback(|e: Event| {
+            Msg::Select(e.target_unchecked_into::<HtmlInputElement>().value())
+        });
+
+        let indicators = INDICATORS
+            .iter()
+            .map(|ind| {
+                html! { <option>{ ind }</option> }
+            })
+            .collect::<Html>();
+
         html! {
             <>
                 <header>
@@ -47,21 +68,28 @@ impl Component for Model {
                 <main>
                     <div id="left">
                         <h1>{ "Tracking Progress Updates" }</h1>
-                        <p>{ "Add an overlay to indicator tiles to let users know they have recently been updated with new data." }</p>
-                        <ul>
-                            <li>{ " Use the 'Add' form to add an overlay for a newly updated indicator. These overlays expire 30 days after their creation."}</li>
-                            <li>{ " Use the 'Remove' form if you want to remove an indicator overlay before the 30 day expiration date. "}</li>
-                        </ul>
+                        <p>{ "An overlay can be added to indicator tiles to let users know they have recently been updated with new data. The overlays will expire after 30 days." }
+                            <ul>
+                                <li>{ " Select the indicator from the dropdown and use the 'Add' button to add an overlay to it."}</li>
+                                <li>{ " Use the 'Remove' button if you want to remove an indicator's overlay before the 30-day expiration. "}</li>
+                            </ul>
+                        </p>
                     </div>
                     <div id="right">
                         <h1>{ "Updated Indicators" }</h1>
                         <p>{ "[Display of current updated indicators here (or message saying none)]"}</p>
                         <hr />
-                        <div id="buttons">
-                            <button class="left">{ "➕ Add Indicator ➠" }</button>
-                            <select class="right" name="add"></select>
-                            <button class="left">{ "➖ Remove Indicator ➠" }</button>
-                            <select class="right" name="remove"></select>
+                        <div id="update-area">
+                            <select {onchange}>
+                                <option SELECTED="true">{"Select Indicator"}</option>
+                                { indicators }
+                            </select>
+                            <button onclick={link.callback(|_| Msg::Add)}>
+                                { "Add It" }
+                            </button>
+                            <button onclick={link.callback(|_| Msg::Remove)}>
+                                { "Remove It" }
+                            </button>
                         </div>
                     </div>
                 </main>
@@ -69,6 +97,23 @@ impl Component for Model {
         }
     }
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Add => match self.indicator {
+                None => log!(format!("Please select an indicator to add.")),
+                Some(_) => log!(format!("{:?}", self.indicator)),
+            },
+            Msg::Remove => match self.indicator {
+                None => log!(format!("Please select an indicator to remove.")),
+                Some(_) => log!(format!("{:?}", self.indicator)),
+            },
+            Msg::Select(ind) => {
+                if !INDICATORS.contains(&ind.as_str()) {
+                    self.indicator = None;
+                } else {
+                    self.indicator = Some(ind);
+                }
+            }
+        };
         true
     }
 }
