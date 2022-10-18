c27aef754e0dc1294c1e8efbb8f81dc071a198c7
Kris Warner <kdwarn@protonmail.com>
2022-10-18 17:25:27 -0400

    Update design; use API to add/rm indicators

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use dotenvy_macro::dotenv;
 use gloo_console::log;
 use gloo_net::http::Request;
@@ -6,31 +8,69 @@ use yew::prelude::*;
 
 const API_BASE: &str = dotenv!("API_BASE");
 
+enum Msg {
+    Add,
+    Remove,
+    Status(String),
+    Select(String),
+    SetCurrentIndicators(Vec<String>),
+}
+
 /// Fetch recently updated indicators from API
 async fn get_updated_indicators() -> Result<Vec<String>, String> {
-    let fetched_ur = Request::get(&format!("{API_BASE}/indicators")).send().await;
+    let r = Request::get(&format!("{API_BASE}/indicators")).send().await;
 
-    match fetched_ur {
+    match r {
         Ok(response) => {
             let json: Result<Vec<String>, _> = response.json().await;
             match json {
-                Ok(v) => Ok(v),
+                Ok(mut v) => {
+                    v.sort();
+                    Ok(v)
+                }
                 Err(e) => Err(e.to_string()),
             }
         }
         Err(e) => Err(e.to_string()),
     }
 }
-enum Msg {
-    Add,
-    Remove,
-    Select(String),
-    SetCurrentIndicators(Vec<String>),
+
+/// Add new indicator
+async fn add_indicator(indicator: String) -> Result<String, String> {
+    let payload = HashMap::from([("name", indicator)]);
+
+    let r = Request::post(&format!("{API_BASE}/indicators"))
+        .json(&payload)
+        .unwrap()
+        .send()
+        .await;
+
+    match r {
+        Ok(_) => Ok("ok".to_string()),
+        Err(e) => Err(e.to_string()),
+    }
+}
+
+/// Delete indicator
+async fn remove_indicator(indicator: String) -> Result<String, String> {
+    let payload = HashMap::from([("name", indicator)]);
+
+    let r = Request::delete(&format!("{API_BASE}/indicators"))
+        .json(&payload)
+        .unwrap()
+        .send()
+        .await;
+
+    match r {
+        Ok(_) => Ok("ok".to_string()),
+        Err(e) => Err(e.to_string()),
+    }
 }
 
 struct Model {
     indicator: Option<String>,
     updated_indicators: Vec<String>,
+    update_status: Option<String>,
 }
 
 const INDICATORS: &[&str] = &[
@@ -67,6 +107,7 @@ impl Component for Model {
         Model {
             indicator: None,
             updated_indicators: vec![],
+            update_status: None,
         }
     }
 
@@ -80,7 +121,13 @@ impl Component for Model {
         let updated_indicators = if self.updated_indicators.is_empty() {
             html! { <p>{ "There are no recently updated indicators."}</p> }
         } else {
-            html! { <p>{ format!("{:#?}", self.updated_indicators) }</p> }
+            html! { <ul> {
+                    self.updated_indicators
+                        .iter()
+                        .map(|ind| html! { <li>{format!("{}", ind)}</li> })
+                        .collect::<Html>()
+                    }
+            </ul>}
         };
 
         let indicators = INDICATORS
@@ -94,22 +141,16 @@ impl Component for Model {
             <>
                 <header>
                     <a href="https://sites.google.com/a/dvrpc.org/webteam/">{ "Intranet" }</a>
+                    <h1>{ "Tracking Progress Updates" }</h1>
                 </header>
                 <main>
                     <div id="left">
-                        <h1>{ "Tracking Progress Updates" }</h1>
                         <p>{ "An overlay can be added to indicator tiles to let users know they have recently been updated with new data. The overlays will expire after 30 days." }
                             <ul>
                                 <li>{ " Select the indicator from the dropdown and use the 'Add' button to add an overlay to it."}</li>
                                 <li>{ " Use the 'Remove' button if you want to remove an indicator's overlay before the 30-day expiration. "}</li>
                             </ul>
                         </p>
-                    </div>
-                    <div id="right">
-                        <h1>{ "Updated Indicators" }</h1>
-
-                        <p class="center">{ updated_indicators }</p>
-                        <hr />
                         <div id="update-area">
                             <select {onchange}>
                                 <option SELECTED="true">{"Select Indicator"}</option>
@@ -122,21 +163,51 @@ impl Component for Model {
                                 { "Remove It" }
                             </button>
                         </div>
+                        if let Some(v) = &self.update_status {
+                            <p>{ format!("{}", v) }</p>
+                        }
+                    </div>
+                    <div id="right">
+                        <div>
+                            <h2>{ "Recently Updated Indicators" }</h2>
+                            { updated_indicators }
+                        </div>
                     </div>
                 </main>
             </>
         }
     }
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::Add => match self.indicator {
-                None => log!(format!("Please select an indicator to add.")),
-                Some(_) => log!(format!("{:?}", self.indicator)),
+            Msg::Add => match self.indicator.clone() {
+                None => (),
+                Some(v) => ctx.link().send_future(async move {
+                    match add_indicator(v).await {
+                        Ok(_) => Msg::Status("Indicator added".to_string()),
+                        Err(e) => Msg::Status(e.to_string()),
+                    }
+                }),
             },
-            Msg::Remove => match self.indicator {
-                None => log!(format!("Please select an indicator to remove.")),
-                Some(_) => log!(format!("{:?}", self.indicator)),
+            Msg::Remove => match self.indicator.clone() {
+                None => (),
+                Some(v) => {
+                    ctx.link().send_future(async move {
+                        match remove_indicator(v).await {
+                            Ok(_) => Msg::Status("Indicator removed".to_string()),
+                            Err(e) => Msg::Status(e.to_string()),
+                        }
+                    });
+                }
             },
+            Msg::Status(status) => {
+                self.update_status = Some(status);
+                ctx.link().send_future(async {
+                    match get_updated_indicators().await {
+                        Ok(v) => Msg::SetCurrentIndicators(v),
+                        Err(e) => Msg::SetCurrentIndicators(vec![e]),
+                    }
+                })
+            }
             Msg::Select(ind) => {
                 if !INDICATORS.contains(&ind.as_str()) {
                     self.indicator = None;
